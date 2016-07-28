bb1fb4d30825959f16d9756e84bf3f0e7d3af8c2
John Walley <john@Walley.org.uk>
2016-07-28 18:16:04 +0100

    Make BumpsChartControls a pure function

--- src/BumpsChartControls.jsx
+++ src/BumpsChartControls.jsx
@@ -4,13 +4,13 @@ import AVFastForward from 'material-ui/svg-icons/av/fast-forward';
 import AVFastRewind from 'material-ui/svg-icons/av/fast-rewind';
 
 import {
-    ShareButtons,
-    generateShareIcon,
+  ShareButtons,
+  generateShareIcon,
 } from 'react-share';
 
 const {
-    FacebookShareButton,
-    TwitterShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
 } = ShareButtons;
 
 const FacebookIcon = generateShareIcon('facebook');
@@ -33,59 +33,41 @@ const twitterShareUrl = window.location;
 const title = 'Share these results';
 const twitterTitle = 'Check out these Cambridge Bumps results!';
 
-export default class BumpsChartControls extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.incrementYear = this.incrementYear.bind(this);
-    this.decrementYear = this.decrementYear.bind(this);
-  }
-
-  incrementYear() {
-    this.props.incrementYear();
-  }
-
-  decrementYear() {
-    this.props.decrementYear();
-  }
-
-  render() {
-    return (
-      <div className="bumpsControls" style={styles.customButtons}>
-        <IconButton onClick={this.decrementYear}>
-          <AVFastRewind color="#91B9A4" />
-        </IconButton>
-        <div className="share">
-          <FacebookShareButton
-            url={facebookShareUrl}
-            title={title}
-            className="facebook-share-button"
-          >
-            <FacebookIcon
-              size={24}
-              round
-            />
-          </FacebookShareButton>
-          <TwitterShareButton
-            url={twitterShareUrl}
-            title={twitterTitle}
-            className="twitter-share-button"
-          >
-            <TwitterIcon
-              size={24}
-              round
-            />
-          </TwitterShareButton>
-        </div>
-        <IconButton onClick={this.incrementYear}>
-          <AVFastForward color="#91B9A4" />
-        </IconButton>
-      </div>
-    );
-  }
-}
+const BumpsChartControls = (props) =>
+  <div className="bumpsControls" style={styles.customButtons}>
+    <IconButton onClick={props.decrementYear}>
+      <AVFastRewind color="#91B9A4" />
+    </IconButton>
+    <div className="share">
+      <FacebookShareButton
+        url={facebookShareUrl}
+        title={title}
+        className="facebook-share-button"
+      >
+        <FacebookIcon
+          size={24}
+          round
+        />
+      </FacebookShareButton>
+      <TwitterShareButton
+        url={twitterShareUrl}
+        title={twitterTitle}
+        className="twitter-share-button"
+      >
+        <TwitterIcon
+          size={24}
+          round
+        />
+      </TwitterShareButton>
+    </div>
+    <IconButton onClick={props.incrementYear}>
+      <AVFastForward color="#91B9A4" />
+    </IconButton>
+  </div>;
 
 BumpsChartControls.propTypes = {
   incrementYear: React.PropTypes.func,
   decrementYear: React.PropTypes.func,
 };
+
+export default BumpsChartControls;

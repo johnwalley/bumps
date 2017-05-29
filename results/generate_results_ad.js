6a584da09c57c29c289b9a192984d7143eaaf970
johnwalley <john@walley.org.uk>
2017-05-29 15:13:34 +0100

    WIP: Read from ad format

---
+++ results/generate_results_ad.js
@@ -0,0 +1,23 @@
+import {read_ad} from 'd3-bumps-chart';
+
+var fs = require('fs');
+
+const events = [];
+
+fs.readdir('./results/ad_format/', function (err, files) {
+    if (err) throw err;
+    var numFiles = 0;
+    files.forEach(function (file) {
+        const contents = fs.readFileSync('./results/ad_format/' + file, 'utf8');
+        const event = read_ad(contents);
+        numFiles++;
+        events.push(event);
+    });
+
+    fs.writeFile('./results/generated.json', JSON.stringify(events), function() {
+        console.log('Scccessfully wrote file to ./generated.json');
+    });
+
+    console.log(`Found ${numFiles} files`);
+});
+
